9978c4b768fd390a8b914d0ac2f39b6dcfe13a78
LeZizzi <43583237+LeZizzi@users.noreply.github.com>
2019-04-17 00:38:31 +0100

    Create kickstart.sh

---
+++ kickstart.sh
@@ -0,0 +1,14 @@
+#shebang or should I say...Bazinga gimme some cloud foundry kit
+
+sudo dnf install bash-completion -y
+
+#I want it all ...Yes Freddy Mercurie style cuz storing tools is cheap ,lookingaround to find one when you got a P1 ain'tmy job ,I just pickthe tool and do open cloud surgery
+
+
+dnf group install --with-optional virtualization -y
+
+#Feel free to slap,blame this end to end automation project should the script ask for input from the Layer 8 of the OSI model.
+
+
+
+
